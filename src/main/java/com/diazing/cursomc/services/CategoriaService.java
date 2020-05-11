8ffcb4c38f557ae346eab299c30f79c56571a497
Wesley Matias <wesley.matiasds@gmail.com>
2020-05-10 23:10:46 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/diazing/cursomc/services/CategoriaService.java
+++ src/main/java/com/diazing/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.diazing.cursomc.domain.Categoria;
 import com.diazing.cursomc.repositories.CategoriaRepository;
+import com.diazing.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,10 +15,10 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
-		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		
-	}
+	public Categoria buscar(Integer id) {  
+		Optional<Categoria> obj = repo.findById(id);  
+		return obj.orElseThrow(() -> new ObjectNotFoundException(    
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); }
+
 
 }
